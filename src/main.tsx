5796b48c0facf8279fc451acf8347036e0f3547a
hugogebel-boop <hugogebel@gmail.com>
2025-09-27 22:47:40 +0200

    update: favicon + contenu + image

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import SponsorDeck from './sponsor/SponsorDeck'
+import faviconUrl from './assets/favicon.svg' // ⬅️ import du favicon (src/assets)
+
+// Ajoute/MAJ la balise <link rel="icon"> avec l'URL packagée par Vite
+function applyFavicon() {
+    let link = document.querySelector<HTMLLinkElement>('link[rel="icon"]')
+    if (!link) {
+        link = document.createElement('link')
+        link.rel = 'icon'
+        document.head.appendChild(link)
+    }
+    link.type = 'image/svg+xml'
+    link.href = faviconUrl
+}
+applyFavicon()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <SponsorDeck />
-  </React.StrictMode>
+    <React.StrictMode>
+        <SponsorDeck />
+    </React.StrictMode>
 )
